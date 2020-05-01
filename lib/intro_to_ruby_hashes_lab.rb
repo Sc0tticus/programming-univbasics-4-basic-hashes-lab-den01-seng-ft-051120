6fe3e64a28ecad12e70af71923216ab8f4a8cdf4
Scott Singer <ssinger303@gmail.com>
2020-05-01 03:29:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,15 +2,15 @@ def new_hash
 {}
 end
 
-def my_hash
-  { :fav_band => "The Beatles" }
-end
+#def my_hash
+#  { :fav_band => "The Beatles" }
+#end
 
-def pioneer
-{ name : "Grace Hopper" }
-end
+#def pioneer
+#{ name : "Grace Hopper" }
+#end
 
 
-def id_hash_generator(number)
-  { id: number }
-end
+#def id_hash_generator(number)
+#  { id: number }
+#end
